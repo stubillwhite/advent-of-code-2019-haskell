c897e6ef94145bc251ac715d4607a624549eee17
Stu W <stubillwhite@gmail.com>
2019-12-01 11:58:24 +0000

    Add solution to day 1 part 2

--- src/AdventOfCode/Day01.hs
+++ src/AdventOfCode/Day01.hs
@@ -1,5 +1,5 @@
 module AdventOfCode.Day01
-  ( day01, solutionOne
+  ( day01, solutionOne, solutionTwo
   ) where 
 
 import System.IO
@@ -17,7 +17,21 @@ solutionOne :: String -> Int
 solutionOne input =
   foldl (+) 0 $ map (fuelRequiredFor . parseInput) (lines input)
 
+-- Part two
+
+totalFuelRequiredFor :: Int -> Int
+totalFuelRequiredFor mass =
+  if fuelRequired <= 0 then 0 else fuelRequired + totalFuelRequiredFor fuelRequired
+  where
+    fuelRequired = fuelRequiredFor mass
+
+solutionTwo :: String -> Int
+solutionTwo input =
+  foldl (+) 0 $ map (totalFuelRequiredFor . parseInput) (lines input)
+
 day01 :: IO ()
 day01 = do  
     problemInput <- readFile "input/day-01.txt"
     putStrLn $ "Part one: " ++ show (solutionOne problemInput)
+    putStrLn $ "Part two: " ++ show (solutionTwo problemInput)
+
--- test/AdventOfCode/Day01Spec.hs
+++ test/AdventOfCode/Day01Spec.hs
@@ -2,7 +2,7 @@ module AdventOfCode.Day01Spec (main, spec) where
 
 import Test.Hspec
 
-import AdventOfCode.Day01 (day01, solutionOne)
+import AdventOfCode.Day01 (day01, solutionOne, solutionTwo)
 
 main :: IO ()
 main = hspec spec
@@ -15,3 +15,8 @@ spec = do
       solutionOne "14" `shouldBe` 2
       solutionOne "1969" `shouldBe` 654
       solutionOne "100756" `shouldBe` 33583
+  describe "solutionTwo" $ do
+    it "should return expected responses for example data" $ do
+      solutionTwo "12" `shouldBe` 2
+      solutionTwo "1969" `shouldBe` 966
+      solutionTwo "100756" `shouldBe` 50346
